e65259cc1cf9e11ac4bbfe678bc063c77a391175
Leonid2377 <leonid2377@mail.ru>
2022-12-19 07:05:41 +0300

    alpha

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseSettings, EmailStr
+from typing import Optional
+
+class Settings(BaseSettings):
+    app_title: str = 'Благотворительный фонд поддрержки котов'
+    database_url: str = 'sqlite+aiosqlite:///./cats.db'
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
